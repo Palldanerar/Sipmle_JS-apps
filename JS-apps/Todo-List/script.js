f6667ba26b997b9f8b6175876bd305f2b1e0ad17
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2022-10-21 21:59:36 +0300

    Updated ToDo-List
    Now you can add tasks using the input field.

--- JS-apps/Todo-List/script.js
+++ JS-apps/Todo-List/script.js
@@ -1,8 +1,20 @@
-let task = document.querySelectorAll('.task')
 let addTask = document.querySelector('.btn')
+let todo = document.querySelector('.todo')
 
+function getEvent() {
+    let task = document.querySelectorAll('.task')
 
-task.forEach(item => item.addEventListener('click', () => {
-    item.classList.toggle("task")
-    item.classList.toggle("task__complite")
-}))
+    task.forEach(item => item.addEventListener('click', () => {
+        item.classList.toggle("task")
+        item.classList.toggle("task__complite")
+    }))
+}
+
+getEvent()
+
+addTask.addEventListener('click', () => {
+    let newTask = document.querySelector('.inputTask').value
+    todo.innerHTML += `<li class="task">${newTask}</li>`
+    getEvent()
+})
+
