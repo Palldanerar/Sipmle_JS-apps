37f58fff6bc8eaab7f8bf6080104a92561899347
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2022-11-01 11:58:48 +0300

    New design for the ToDo-List

--- JS-apps/Todo-List/script.js
+++ JS-apps/Todo-List/script.js
@@ -1,20 +1,69 @@
-let addTask = document.querySelector('.btn')
-let todo = document.querySelector('.todo')
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new__task__form");
+	const input = document.querySelector("#new__task__input");
+	const list_el = document.querySelector("#tasks");
 
-function getEvent() {
-    let task = document.querySelectorAll('.task')
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-    task.forEach(item => item.addEventListener('click', () => {
-        item.classList.toggle("task")
-        item.classList.toggle("task__complite")
-    }))
-}
+		const task = input.value;
 
-getEvent()
+        if (!task) {
+            input.placeholder = 'This field cannot be empty!'
+            return
+        }
 
-addTask.addEventListener('click', () => {
-    let newTask = document.querySelector('.inputTask').value
-    todo.innerHTML += `<li class="task">${newTask}</li>`
-    getEvent()
-})
+        input.placeholder = 'What do you have planned?'
 
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
