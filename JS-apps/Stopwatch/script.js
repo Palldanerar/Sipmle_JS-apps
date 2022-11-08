abd59df70278c711313a6a5a0c0e36853241f8de
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2022-11-08 09:49:03 +0300

    New app - Stopwatch

---
+++ JS-apps/Stopwatch/script.js
@@ -0,0 +1,60 @@
+const time_el = document.querySelector('.watch .time')
+const block_intervals = document.querySelector('.block__intervals')
+const interval_el = document.querySelector('.intervals')
+const start_btn = document.getElementById('start')
+const stop_btn = document.getElementById('stop')
+const interval_btn = document.getElementById('interval')
+const reset_btn = document.getElementById('reset')
+
+let seconds = 0
+let interval = null
+
+start_btn.addEventListener('click', start)
+stop_btn.addEventListener('click', stop)
+reset_btn.addEventListener("click", reset)
+interval_btn.addEventListener('click', intervalTime)
+
+function timer() {
+    seconds++
+
+    let hrs = Math.floor(seconds / 3600)
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60)
+    let secs = seconds % 60
+
+    if (secs < 10) secs = "0" + secs
+    if (mins < 10) mins = "0" + mins
+    if (hrs < 10) hrs = "0" + hrs
+
+    time_el.innerHTML = `${hrs}:${mins}:${secs}`
+
+}
+
+function start() {
+    if (interval) {
+        return
+    }
+
+    interval = setInterval(timer, 1000)
+}
+
+function stop() {
+    clearInterval(interval)
+    interval = null
+}
+
+function reset() {
+    stop()
+    seconds = 0
+    time_el.innerHTML = "00:00:00"
+    block_intervals.style.display = 'none'
+    interval_el.innerHTML = ''
+
+}
+
+function intervalTime() {
+    if (block_intervals) {
+        block_intervals.style.display = 'block'
+    }
+
+    interval_el.innerHTML += `<p>${time_el.textContent}</p>`
+}
